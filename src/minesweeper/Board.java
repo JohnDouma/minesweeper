ad6905071f297bacafa0b10c1526f30769459fe4
John Douma <jdouma@amazon.com>
2017-04-15 15:02:07 -0400

    Implemented checkRep function to ensure number of adjacent bombs is updated; only perform a dig operation on a square if it is in an untouched state.

--- src/minesweeper/Board.java
+++ src/minesweeper/Board.java
@@ -3,7 +3,6 @@
  */
 package minesweeper;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,47 +10,52 @@ import java.util.Queue;
  * TODO: Specification
  */
 public class Board {
-    
-    // Abstraction Function: A 2D array of squares represents a Minesweeper board
 
-    // Representation Invariant: The grid is never null and has at least one row 
+    // Abstraction Function: A 2D array of squares represents a Minesweeper
+    // board
+
+    // Representation Invariant: The grid is never null and has at least one row
     // and one column. All rows are the same size. Each square of the grid holds
     // the value of the number of adjacent bombs.
-    
-    // Representation Exposure Argument: The array of squares is not accessible to clients. 
+
+    // Representation Exposure Argument: The array of squares is not accessible
+    // to clients.
     // The state of the board is only viewed through its toString method.
-    
-    // Thread Safety Argument: All public non-constructor methods are synchronized
-    
-    private enum State {UNTOUCHED, DUG, FLAGGED};
-    
+
+    // Thread Safety Argument: All public non-constructor methods are
+    // synchronized
+
+    private enum State {
+        UNTOUCHED, DUG, FLAGGED
+    };
+
     private static final int DEFAULT_SIZE = 10;
     private static final float BOMB_PERCENTAGE = 0.25f;
-    
+
     private class Square {
         private boolean hasBomb;
         private State state;
         private int neighborsWithBombs;
-        
+
         public Square() {
             hasBomb = false;
             state = State.UNTOUCHED;
         }
-        
+
         public boolean dig() {
-            if (state != State.DUG) {
+            if (state == State.UNTOUCHED) {
                 state = State.DUG;
                 return hasBomb;
             }
             return false;
         }
-        
+
         public void flag() {
             if (state == State.UNTOUCHED) {
                 state = State.FLAGGED;
             }
         }
-        
+
         public void deflag() {
             if (state == State.FLAGGED) {
                 state = State.DUG;
@@ -70,26 +74,27 @@ public class Board {
             case UNTOUCHED:
                 retval = "-";
             }
-            
+
             return retval;
         }
     }
-    
+
     private class Pair {
         public Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
+
         private int x;
         private int y;
     }
-    
+
     private volatile Square[][] grid;
-    
+
     public Board() {
         this(DEFAULT_SIZE, DEFAULT_SIZE);
     }
-    
+
     public Board(int rows, int cols) {
         grid = new Square[rows][cols];
         for (int row = 0; row < grid.length; row++) {
@@ -98,17 +103,19 @@ public class Board {
                 grid[row][col].hasBomb = (Math.random() < BOMB_PERCENTAGE);
             }
         }
-        
-        // TODO checkRep() should adjust the bombsWithNeighbors values
+
+        checkRep();
     }
-    
+
     /**
-     * Attempts to dig at a board location. If the square specified
-     * by x and y is valid and in an undug state, sets the state of the
-     * square to DUG and removes the bomb if it contains one.
+     * Attempts to dig at a board location. If the square specified by x and y
+     * is valid and in an undug state, sets the state of the square to DUG and
+     * removes the bomb if it contains one.
      * 
-     * @param x column of grid
-     * @param y row of grid
+     * @param x
+     *            column of grid
+     * @param y
+     *            row of grid
      * 
      * @return true if dug square contains a bomb, false otherwise
      */
@@ -116,65 +123,86 @@ public class Board {
         boolean retval = false;
         if (isValidGridLocation(x, y)) {
             retval = grid[y][x].dig();
-            updateNeighbors(x, y);
+            if (retval) {
+                grid[y][x].hasBomb = false;
+            }
+            checkRep();
         }
-        
+
         return retval;
     }
-    
+
     /**
      * If the square specified is untouched, changes its state to flagged.
      * 
-     * @param x column of grid
-     * @param y row of grid
+     * @param x
+     *            column of grid
+     * @param y
+     *            row of grid
      */
     public synchronized void flag(int x, int y) {
-        throw new UnsupportedOperationException();
+        if (isValidGridLocation(x, y)) {
+            grid[y][x].flag();
+        }
     }
-    
+
     /**
      * If the specified square is flagged, changes its state to untouched.
      * 
-     * @param x column of grid
-     * @param y row of grid
+     * @param x
+     *            column of grid
+     * @param y
+     *            row of grid
      */
     public synchronized void deflag(int x, int y) {
-        throw new UnsupportedOperationException();
+        if (isValidGridLocation(x, y)) {
+            grid[y][x].deflag();
+        }
     }
 
     @Override
     public synchronized String toString() {
         StringBuffer sb = new StringBuffer();
         for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[row].length-1; col++)
-            {
+            for (int col = 0; col < grid[row].length - 1; col++) {
                 sb.append(grid[row][col]);
                 sb.append(" ");
             }
             sb.append(grid[row][grid[row].length - 1]);
             sb.append(System.lineSeparator());
         }
-        
+
         return sb.toString();
     }
-    
+
     /*
-     * Returns true if x and y specify a valid location on the grid.
-     * x represents the column and y represents the row.
+     * Ensures the representation invariant. Updates all squares with the number
+     * of neighboring bombs.
+     */
+    private void checkRep() {
+        Queue<Pair> queue;
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                int numBombs = 0;
+                queue = generateNeighbors(col, row);
+                for (Pair coord : queue) {
+                    if (grid[coord.y][coord.x].hasBomb) {
+                        numBombs++;
+                    }
+                }
+                grid[row][col].neighborsWithBombs = numBombs;
+            }
+        }
+    }
+
+    /*
+     * Returns true if x and y specify a valid location on the grid. x
+     * represents the column and y represents the row.
      */
     private boolean isValidGridLocation(int x, int y) {
-        return x >= 0 && y >= 0 && x <= grid[0].length && y <= grid.length ;
+        return x >= 0 && y >= 0 && x <= grid[0].length && y <= grid.length;
     }
-    
-    /*
-     * If the specified grid location contains no neighbors with bombs then
-     * change the state of all untouched neighbors to dug and recursively perform
-     * this step for each of their neighbors.
-     */
-    private void updateNeighbors(int x, int y) {
-        throw new UnsupportedOperationException();
-    }
-    
+
     /*
      * Returns a queue of pairs of grid indexes
      */
@@ -182,25 +210,29 @@ public class Board {
         Queue<Pair> queue = new LinkedList<Pair>();
         if (x > 0) {
             queue.add(new Pair(x - 1, y));
-            if (y > 0) queue.add(new Pair(x-1, y-1));
-            if (y < grid[0].length-1) queue.add(new Pair(x-1, y+1));
+            if (y > 0)
+                queue.add(new Pair(x - 1, y - 1));
+            if (y < grid[0].length - 1)
+                queue.add(new Pair(x - 1, y + 1));
         }
-        
+
         if (x < grid.length - 1) {
             queue.add(new Pair(x + 1, y));
-            if (y > 0) queue.add(new Pair(x+1, y-1));
-            if (y < grid[0].length-1) queue.add(new Pair(x+1, y+1));
+            if (y > 0)
+                queue.add(new Pair(x + 1, y - 1));
+            if (y < grid[0].length - 1)
+                queue.add(new Pair(x + 1, y + 1));
         }
-        
+
         if (y > 0) {
             queue.add(new Pair(x, y - 1));
         }
-        
+
         if (y < grid[0].length - 1) {
             queue.add(new Pair(x, y + 1));
         }
-        
+
         return queue;
     }
-    
+
 }
